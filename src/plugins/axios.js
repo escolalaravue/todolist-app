37912ab9349406c62236d5f78c159456b45e0e00
Tiago Matos <tiago@tiagomatos.com>
2024-08-01 13:11:21 +1000

    Adicionado api url env var

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import Vue from 'vue';
 import Cookie from '@/service/cookie';
 
-axios.defaults.baseURL = 'http://api.todolist.test/api';
+axios.defaults.baseURL = process.env.API_URL;
 axios.defaults.headers.common['Content-Type'] = 'application/json';
 axios.defaults.headers.common['Accept'] = 'application/json';
 
